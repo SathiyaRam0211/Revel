c994496926bcdda2c19d81aa2a81b72a1efed281
Gunalan Lakshmanan <gunameha@gmail.com>
2024-07-31 15:16:31 +0530

    G-Add August schedule

--- src/constants/events.jsx
+++ src/constants/events.jsx
@@ -426,5 +426,237 @@ export const eventDetails = {
         },
       ],
     },
+    {
+      day: 6,
+      events: [
+        {
+          category: "house",
+          venue: "Kickers Academy, Madipakkam",
+          faculty: "Joshi",
+          time: "8:00 - 9:00 pm",
+        },
+      ],
+    },
+    {
+      day: 8,
+      events: [
+        {
+          category: "creativeMovement",
+          venue: "Kickers Academy, Madipakkam",
+          faculty: "Joshi",
+          time: "8:00 - 9:00 pm",
+        },
+        {
+          category: "locking",
+          venue: "Kickers Academy, Madipakkam",
+          faculty: "Sakthi",
+          time: "9:00 - 10:00 pm",
+        },
+      ],
+    },
+    {
+      day: 10,
+      events: [
+        {
+          category: "hipHop",
+          venue: "Kickers Academy, Madipakkam",
+          faculty: "DeeZee aka Dinesh",
+          time: "7:00 - 8:00 am",
+        },
+        {
+          category: "hipHop",
+          venue: "Kickers Academy, Madipakkam",
+          faculty: "DeeZee aka Dinesh",
+          time: "8:00 - 9:00 am",
+        },
+        {
+          category: "breaking",
+          venue: "Hips n Toez, T Nagar",
+          faculty: "Feelhips aka Prabhu",
+          time: "7:00 - 8:00 pm",
+        },
+      ],
+    },
+    {
+      day: 13,
+      events: [
+        {
+          category: "hipHop",
+          venue: "Hips n Toez, T Nagar",
+          faculty: "DeeZee aka Dinesh",
+          time: "7:00 - 8:00 am",
+        },
+        {
+          category: "hipHop",
+          venue: "Hips n Toez, T Nagar",
+          faculty: "DeeZee aka Dinesh",
+          time: "8:00 - 9:00 am",
+        },
+      ],
+    },
+    {
+      day: 15,
+      events: [
+        {
+          category: "house",
+          venue: "Kickers Academy, Madipakkam",
+          faculty: "Joshi",
+          time: "8:00 - 9:00 pm",
+        },
+        {
+          category: "locking",
+          venue: "Kickers Academy, Madipakkam",
+          faculty: "Sakthi",
+          time: "9:00 - 10:00 pm",
+        },
+      ],
+    },
+    {
+      day: 20,
+      events: [
+        {
+          category: "hipHop",
+          venue: "Dance Lessons, Mogappair",
+          faculty: "DeeZee aka Dinesh",
+          time: "8:00 - 9:00 pm",
+        },
+        {
+          category: "hipHop",
+          venue: "Dance Lessons, Mogappair",
+          faculty: "DeeZee aka Dinesh",
+          time: "9:00 - 10:00 pm",
+        },
+      ],
+    },
+    {
+      day: 21,
+      events: [
+        {
+          category: "graffiti",
+          venue: "Kickers Academy, Madipakkam",
+          faculty: "Vijay",
+          time: "8:00 - 9:00 pm",
+        },
+      ],
+    },
+    {
+      day: 22,
+      events: [
+        {
+          category: "creativeMovement",
+          venue: "Kickers Academy, Madipakkam",
+          faculty: "Joshi",
+          time: "8:00 - 9:00 pm",
+        },
+        {
+          category: "locking",
+          venue: "Kickers Academy, Madipakkam",
+          faculty: "Sakthi",
+          time: "9:00 - 10:00 pm",
+        },
+      ],
+    },
+    {
+      day: 24,
+      events: [
+        {
+          category: "locking",
+          venue: "Hips n Toez, T Nagar",
+          faculty: "Sakthi",
+          time: "7:30 - 8:30 am",
+        },
+        {
+          category: "breaking",
+          venue: "Hips n Toez, T Nagar",
+          faculty: "Feelhips aka Prabhu",
+          time: "7:00 - 8:00 pm",
+        },
+      ],
+    },
+    {
+      day: 26,
+      events: [
+        {
+          category: "graffiti",
+          venue: "Dance Lessons, Mogappair",
+          faculty: "Vijay",
+          time: "8:30 - 9:30 pm",
+        },
+      ],
+    },
+    {
+      day: 27,
+      events: [
+        {
+          category: "locking",
+          venue: "Kickers Academy, Madipakkam",
+          faculty: "Sakthi",
+          time: "8:00 - 9:00 pm",
+        },
+        {
+          category: "house",
+          venue: "Kickers Academy, Madipakkam",
+          faculty: "Joshi",
+          time: "9:00 - 10:00 pm",
+        },
+      ],
+    },
+    {
+      day: 29,
+      events: [
+        {
+          category: "hipHop",
+          venue: "Dance Lessons, Mogappair",
+          faculty: "DeeZee aka Dinesh",
+          time: "8:00 - 9:00 pm",
+        },
+        {
+          category: "hipHop",
+          venue: "Dance Lessons, Mogappair",
+          faculty: "DeeZee aka Dinesh",
+          time: "9:00 - 10:00 pm",
+        },
+      ],
+    },
+    {
+      day: 30,
+      events: [
+        {
+          category: "creativeMovement",
+          venue: "RMC, Ashok Nagar",
+          faculty: "Joshi",
+          time: "7:00 - 8:00 am",
+        },
+        {
+          category: "locking",
+          venue: "RMC, Ashok Nagar",
+          faculty: "Sakthi",
+          time: "8:00 - 9:00 am",
+        },
+      ],
+    },
+    {
+      day: 31,
+      events: [
+        {
+          category: "graffiti",
+          venue: "Hips n Toez, T Nagar",
+          faculty: "Vijay",
+          time: "7:30 - 8:30 am",
+        },
+        {
+          category: "graffiti",
+          venue: "Hips n Toez, T Nagar",
+          faculty: "Vijay",
+          time: "8:30 - 9:30 am",
+        },
+        {
+          category: "breaking",
+          venue: "Hips n Toez, T Nagar",
+          faculty: "Feelhips aka Prabhu",
+          time: "7:00 - 8:00 pm",
+        },
+      ],
+    },
   ]
 };
